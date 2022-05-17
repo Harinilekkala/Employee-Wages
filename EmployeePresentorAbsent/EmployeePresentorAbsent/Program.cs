b045d71eef2b5a90691954b47e5ee75b47257e3c
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-17 21:23:04 +0530

    [Harini]: usecase1 program for employee present or absent

---
+++ EmployeePresentorAbsent/EmployeePresentorAbsent/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+namespace EmployeePresentorAbsent
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            presentorabsent presentabsent = new presentorabsent();
+            presentabsent.checkStatus();
+
+            
+        }
+    }
+}
---
+++ EmployeePresentorAbsent/EmployeePresentorAbsent/presentorabsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePresentorAbsent
+{
+    class presentorabsent
+
+    {
+        public void checkStatus()
+        {
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is present" + "\n");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent" + "\n");
+            }
+        }
+    }
+}
